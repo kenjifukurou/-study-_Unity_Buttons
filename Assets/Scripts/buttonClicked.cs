ac5fc5e4f678a3e62d2c105cbb6b9e41b6ea56c6
kenjifukurou <kenjifukurou@gmail.com>
2021-04-17 00:52:53 +0800

    Add Buttons test project

---
+++ Assets/Scripts/buttonClicked.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class buttonClicked : MonoBehaviour
+{
+    public Text timerByButton;
+    private float countdownTime;
+    //public float timeMinusBySecond = 0.01f;
+    private bool buttonStatus = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownTime = 3.00f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonStatus)
+        {
+            countdownTime -= (float)(Time.deltaTime * 0.01);
+            timerByButton.text = countdownTime.ToString("#.00");
+        }
+        
+    }
+
+    public void CreateClock() {
+        buttonStatus = true;
+    }
+    
+}
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = 0;
+        timerText.text = "Count me out: " + countDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown = countDown - Time.deltaTime;
+        timerText.text = "Count me out: " + countDown.ToString("#.00");
+    }
+}
