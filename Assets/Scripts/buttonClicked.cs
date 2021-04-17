1ea830de8fbd574a2914be1db3bff503fa5a2101
kenjifukurou <kenjifukurou@gmail.com>
2021-04-17 19:59:54 +0800

    Various interactable UI elements
    Toggle, Slider, ScrollBar, Dropdown, InputField, ScrollView(ScrollRect) and how to use and script them.

--- Assets/Scripts/buttonClicked.cs
+++ Assets/Scripts/buttonClicked.cs
@@ -10,11 +10,26 @@ public class buttonClicked : MonoBehaviour
     private float countdownTime;
     //public float timeMinusBySecond = 0.01f;
     private bool buttonStatus = false;
+    public Toggle toggleButton;
+
+    public Camera cam;
+    public Slider sliderCam;
+
+    public Dropdown dropdownItems;
+    public GameObject go1;
+
+    public InputField userInput;
+    public Text userName;
+
+    public ScrollRect scrollRectItems;
 
     // Start is called before the first frame update
     void Start()
     {
         countdownTime = 3.00f;
+        toggleButton.isOn = false;
+
+        cam.fieldOfView = sliderCam.value;
     }
 
     // Update is called once per frame
@@ -25,11 +40,46 @@ public class buttonClicked : MonoBehaviour
             countdownTime -= (float)(Time.deltaTime * 0.01);
             timerByButton.text = countdownTime.ToString("#.00");
         }
+
+        if (toggleButton.isOn == true)
+        {
+            CreateClock();
+        } else
+        {
+            buttonStatus = false;
+        }
         
     }
 
     public void CreateClock() {
         buttonStatus = true;
     }
-    
+
+    public void UpdateCamFOVwithSlider() {
+        cam.fieldOfView = sliderCam.value;
+    }
+
+    public void changesObjectSize() {
+
+        switch (dropdownItems.value)
+        {
+            case 0:
+                go1.transform.localScale = new Vector3(1f, 1f, 1f);
+                break;
+            case 1:
+                go1.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                break;
+            case 2:
+                go1.transform.localScale = new Vector3(2f, 2f, 2f);
+                break;
+            default:
+                go1.transform.localScale = new Vector3(1f, 1f, 1f);
+                break;
+        }
+    }
+
+    public void UpdateUserName() {
+        userName.text = userInput.text;
+    }
+
 }
